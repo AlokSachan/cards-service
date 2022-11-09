f4415ea348df8f10e81efc1cfc0ac1ec7166bf80
AlokSachan <alok.sachan.83@gmail.com>
2022-11-09 11:15:27 +0530

    configure spring-cloud-config in cards-service

--- src/main/java/com/alok/cards/controller/CardsController.java
+++ src/main/java/com/alok/cards/controller/CardsController.java
@@ -2,8 +2,13 @@ package com.alok.cards.controller;
 
 
 import com.alok.cards.adaptor.CardsAdaptor;
+import com.alok.cards.config.CardsServiceConfig;
 import com.alok.cards.model.CardsDto;
+import com.alok.cards.model.PropertiesDto;
 import com.alok.cards.service.CardsService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,8 +27,21 @@ public class CardsController {
     @Autowired
     private CardsAdaptor cardsAdaptor;
 
+    @Autowired
+    private CardsServiceConfig cardsServiceConfig;
+
     @GetMapping("/myCards/{customerId}")
     public ResponseEntity<List<CardsDto>> getCardDetails(@PathVariable Integer customerId) {
         return new ResponseEntity<>(cardsAdaptor.adapt(cardsService.findByCustomerId(customerId)), HttpStatus.OK);
+    }
+
+    @GetMapping("/myCards/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow= new ObjectMapper().writer().withDefaultPrettyPrinter();
+        PropertiesDto properties= PropertiesDto.builder().msg(cardsServiceConfig.getMsg())
+                .buildVersion(cardsServiceConfig.getBuildVersion()).mailDetails(cardsServiceConfig.getMailDetails())
+                .activeBranches(cardsServiceConfig.getActiveBranches()).build();
+        String jsonStr = ow.writeValueAsString(properties);
+        return jsonStr;
     }
 }
---
+++ src/main/java/com/alok/cards/model/PropertiesDto.java
@@ -0,0 +1,23 @@
+package com.alok.cards.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Map;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PropertiesDto {
+
+    private String msg;
+    private String buildVersion;
+    private Map<String, String> mailDetails;
+    private List<String> activeBranches;
+
+}
