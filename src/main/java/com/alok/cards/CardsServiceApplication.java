80bd89503b608ed447be52ec1ab1108ae568dac1
AlokSachan <alok.sachan.83@gmail.com>
2022-11-10 16:47:11 +0530

    detect updated property value using refeshScope

--- src/main/java/com/alok/cards/CardsServiceApplication.java
+++ src/main/java/com/alok/cards/CardsServiceApplication.java
@@ -2,8 +2,10 @@ package com.alok.cards;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 public class CardsServiceApplication {
 
 	public static void main(String[] args) {
