d9083dc433a7850c6544c4e54af690e0a099c41a
AlokSachan <alok.sachan.83@gmail.com>
2022-11-18 17:12:36 +0530

    refactor service for cloud gateway

--- src/main/java/com/alok/cards/controller/CardsController.java
+++ src/main/java/com/alok/cards/controller/CardsController.java
@@ -9,16 +9,19 @@ import com.alok.cards.service.CardsService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@Slf4j
 public class CardsController {
 
     @Autowired
@@ -31,7 +34,9 @@ public class CardsController {
     private CardsServiceConfig cardsServiceConfig;
 
     @GetMapping("/myCards/{customerId}")
-    public ResponseEntity<List<CardsDto>> getCardDetails(@PathVariable Integer customerId) {
+    public ResponseEntity<List<CardsDto>> getCardDetails(@PathVariable Integer customerId,
+                                                         @RequestHeader("eazybank-correlation-id")  String correlationId) {
+        log.info("correlationId {}", correlationId);
         return new ResponseEntity<>(cardsAdaptor.adapt(cardsService.findByCustomerId(customerId)), HttpStatus.OK);
     }
 
